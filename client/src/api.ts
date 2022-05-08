c22bbc3e8ca509a06e01b99b45b280fb666ad627
Stoyan Revov <st.revov@gmail.com>
2022-05-08 22:23:16 +0300

    Fix base href in production

--- client/src/api.ts
+++ client/src/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 axios.defaults.baseURL =
   process.env.NODE_ENV === 'development'
     ? `${window.location.protocol}//${window.location.hostname}:8080/api/`
-    : `api/`;
+    : `/api/`;
 
 axios.defaults.headers.common['Content-Type'] = 'application/json';
